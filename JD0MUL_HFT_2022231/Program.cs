52d5a40b67214dcd3bd10a1e829ccafd54150783
FabryUr <FabryUr@FABRYUR>
2022-10-06 12:12:37 +0200

    Add project files.

---
+++ JD0MUL_HFT_2022231/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JD0MUL_HFT_2022231
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
