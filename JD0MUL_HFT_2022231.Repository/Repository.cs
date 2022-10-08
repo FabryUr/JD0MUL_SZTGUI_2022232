4a6ad0a7b3bc4369c6ccd2751ddb10b1b3e6d6bb
FabryUr <fabry.laszlo.laszlo@gmail.com>
2022-10-08 19:07:28 +0200

    Interface and base Repository created

---
+++ JD0MUL_HFT_2022231.Repository/Repository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JD0MUL_HFT_2022231.Repository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected TvShowDbContext ctx;
+
+        public Repository (TvShowDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+        public abstract T Read(int id);
+        public abstract void Update(T item);
+    }
+}
