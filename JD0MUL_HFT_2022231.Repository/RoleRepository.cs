468e7dc0017386e6d11726ca8f7449ee4e8478bb
FabryUr <fabry.laszlo.laszlo@gmail.com>
2022-10-08 19:18:54 +0200

    RoleRepository created

---
+++ JD0MUL_HFT_2022231.Repository/RoleRepository.cs
@@ -0,0 +1,31 @@
+﻿using JD0MUL_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JD0MUL_HFT_2022231.Repository
+{
+    internal class RoleRepository : Repository<Role>, IRepository<Role>
+    {
+        public RoleRepository(TvShowDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Role Read(int id)
+        {
+            return ctx.Roles.FirstOrDefault(t => t.RoleId == id);
+        }
+
+        public override void Update(Role item)
+        {
+            var old = Read(item.RoleId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
