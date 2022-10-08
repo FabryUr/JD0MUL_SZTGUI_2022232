2b543f01cca203aa5bc8edd98d00f3c81eeaac3b
FabryUr <fabry.laszlo.laszlo@gmail.com>
2022-10-08 20:04:01 +0200

    TvShowLogic CRUD complete

---
+++ JD0MUL_HFT_2022231.Logic/TvShowLogic.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using JD0MUL_HFT_2022231.Models;
+using JD0MUL_HFT_2022231.Repository;
+
+namespace JD0MUL_HFT_2022231.Logic
+{
+    public class TvShowLogic
+    {
+        IRepository<TvShow> repository;
+
+        public TvShowLogic(IRepository<TvShow> repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Create(TvShow item)
+        {
+            if (item.Title.Length <3)//because the "YOU" TVshow is 3 letter long, shorter is not accepted
+            {
+                throw new ArgumentException("Title is too short!");
+            }
+            this.repository.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repository.Delete(id);
+        }
+
+        public TvShow Read(int id)
+        {
+            var tvShow = this.repository.Read(id);
+            if (tvShow == null)
+            {
+                throw new ArgumentException("TvShow does not exist");
+            }
+            return this.repository.Read(id);
+        }
+
+        public IQueryable<TvShow> ReadAll()
+        {
+            return repository.ReadAll();
+        }
+
+        public void Update(TvShow item)
+        {
+            this.repository.Update(item);
+        }
+    }
+}
--- JD0MUL_HFT_2022231/Program.cs
+++ JD0MUL_HFT_2022231/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using JD0MUL_HFT_2022231.Models;
-using JD0MUL_HFT_2022231.Repository;
 
 namespace JD0MUL_HFT_2022231
 {
@@ -10,9 +9,7 @@ namespace JD0MUL_HFT_2022231
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            TvShowDbContext ctx = new TvShowDbContext();
-            var items = ctx.TvShows.ToArray();
-            ;
+
         }
     }
 }
