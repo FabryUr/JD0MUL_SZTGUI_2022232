ceb8eec16dd19ffac6ab8b22e869ac046ee57166
FabryUr <fabry.laszlo.laszlo@gmail.com>
2022-10-08 19:18:45 +0200

    ActorRepository created

---
+++ JD0MUL_HFT_2022231.Repository/ActorRepository.cs
@@ -0,0 +1,31 @@
+﻿using JD0MUL_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JD0MUL_HFT_2022231.Repository
+{
+    public class ActorRepository : Repository<Actor>, IRepository<Actor>
+    {
+        public ActorRepository(TvShowDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Actor Read(int id)
+        {
+            return ctx.Actors.FirstOrDefault(t => t.ActorId == id);
+        }
+
+        public override void Update(Actor item)
+        {
+            var old = Read(item.ActorId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
