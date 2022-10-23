ddb887fda2e2724e3c23bd302ebc2f4bf541905a
FabryUr <fabry.laszlo.laszlo@gmail.com>
2022-10-23 12:23:35 +0200

    ActorLogicTester setup complete

---
+++ JD0MUL_HFT_2022231.Test/ActorLogicTester.cs
@@ -0,0 +1,31 @@
+﻿using JD0MUL_HFT_2022231.Logic;
+using JD0MUL_HFT_2022231.Models;
+using JD0MUL_HFT_2022231.Repository;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JD0MUL_HFT_2022231.Test
+{
+    [TestFixture]
+    public class ActorLogicTester
+    {
+        ActorLogic logic;
+        Mock<IRepository<Actor>> mockActorRepo;
+        [SetUp]
+        public void Init()
+        {
+            mockActorRepo = new Mock<IRepository<Actor>>();
+            mockActorRepo.Setup(s => s.ReadAll()).Returns(new List<Actor>()
+            {
+                new Actor("1#1#1#RoleA"),
+                new Actor("2#2#2#RoleB"),
+                new Actor("3#3#3#RoleC"),
+                new Actor("4#4#4#RoleD")
+            }.AsQueryable());
+            logic = new ActorLogic(mockActorRepo.Object);
+        }
+    }
+}
