42c1c05cd2a04cd143d03a9fc3de3a4bb80a4760
FabryUr <fabry.laszlo.laszlo@gmail.com>
2022-10-23 12:06:28 +0200

    TvShowLogicTester setup complete

---
+++ JD0MUL_HFT_2022231.Test/TvShowLogicTester.cs
@@ -0,0 +1,33 @@
+﻿using JD0MUL_HFT_2022231.Logic;
+using JD0MUL_HFT_2022231.Models;
+using JD0MUL_HFT_2022231.Repository;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JD0MUL_HFT_2022231.Test
+{
+    [TestFixture]
+    public class TvShowLogicTester
+    {
+        TvShowLogic logic;
+        Mock<IRepository<TvShow>> mockTvShowRepo;
+        [SetUp]
+        public void Init()
+        {
+            mockTvShowRepo = new Mock<IRepository<TvShow>>();
+            mockTvShowRepo.Setup(m => m.ReadAll()).Returns(new List<TvShow>()
+            {
+                new TvShow("1#TvShowA#2#2004#2008#9,4"),
+                new TvShow("2#TvShowB#1#2019#2020#6,5"),
+                new TvShow("3#TvShowC#1#2006#2010#8"),
+                new TvShow("4#TvShowD#1#2007#2010#10")
+            }.AsQueryable());
+            logic= new TvShowLogic(mockTvShowRepo.Object);
+        }
+    }
+}
