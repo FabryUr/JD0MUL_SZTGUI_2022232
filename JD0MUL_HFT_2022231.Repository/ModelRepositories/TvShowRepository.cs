f7dcab7f1a34f5346a646ad4b2f49e097eaf74a7
FabryUr <fabry.laszlo.laszlo@gmail.com>
2022-11-17 21:15:30 +0100

    Update method fixed

--- JD0MUL_HFT_2022231.Repository/ModelRepositories/TvShowRepository.cs
+++ JD0MUL_HFT_2022231.Repository/ModelRepositories/TvShowRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,7 +24,10 @@ namespace JD0MUL_HFT_2022231.Repository
             var old = Read(item.TvShowId);
             foreach (var prop in old.GetType().GetProperties())
             {
-                prop.SetValue(old, prop.GetValue(item));
+                if (prop.GetAccessors().FirstOrDefault(t => t.IsVirtual) == null)
+                {
+                    prop.SetValue(old, prop.GetValue(item));
+                }
             }
             ctx.SaveChanges();
         }
