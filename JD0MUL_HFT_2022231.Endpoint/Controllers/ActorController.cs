27a866b3b7d5c9a673b1f3467dc2a8ba5c8f5437
LaszloFabry <lfabry@edu.lapinamk.fi>
2023-04-23 16:49:05 +0300

    ActorController updated to SignalR communication

--- JD0MUL_HFT_2022231.Endpoint/Controllers/ActorController.cs
+++ JD0MUL_HFT_2022231.Endpoint/Controllers/ActorController.cs
@@ -1,6 +1,9 @@
-﻿using JD0MUL_HFT_2022231.Logic.Interfaces;
+﻿using JD0MUL_HFT_2022231.Endpoint.Services;
+using JD0MUL_HFT_2022231.Logic.Interfaces;
 using JD0MUL_HFT_2022231.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,10 +15,12 @@ namespace JD0MUL_HFT_2022231.Endpoint.Controllers
     public class ActorController : ControllerBase
     {
         IActorLogic logic;
+        IHubContext<SignalRHub> hub;
 
-        public ActorController(IActorLogic logic)
+        public ActorController(IActorLogic logic, IHubContext<SignalRHub> hub)
         {
             this.logic = logic;
+            this.hub = hub;
         }
 
         // GET: api/<ActorController>
@@ -37,6 +42,7 @@ namespace JD0MUL_HFT_2022231.Endpoint.Controllers
         public void Create([FromBody] Actor value)
         {
             this.logic.Create(value);
+            this.hub.Clients.All.SendAsync("ActorCreated", value);
         }
 
         // PUT api/<ActorController>/5
@@ -44,13 +50,16 @@ namespace JD0MUL_HFT_2022231.Endpoint.Controllers
         public void Update([FromBody] Actor value)
         {
             this.logic.Update(value);
+            this.hub.Clients.All.SendAsync("ActorUpdated", value);
         }
 
         // DELETE api/<ActorController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var actorToDelete = this.logic.Read(id);
             this.logic.Delete(id);
+            this.hub.Clients.All.SendAsync("ActorDeleted", actorToDelete);
         }
     }
 }
