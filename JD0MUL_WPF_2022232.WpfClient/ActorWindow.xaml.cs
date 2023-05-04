10071728e94ed03042fff3ac64f82758be93ca70
LaszloFabry <lfabry@edu.lapinamk.fi>
2023-05-04 11:58:39 +0300

    ActorWindow completed

---
+++ JD0MUL_WPF_2022232.WpfClient/ActorWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JD0MUL_WPF_2022232.WpfClient
+{
+    /// <summary>
+    /// Interaction logic for ActorWindow.xaml
+    /// </summary>
+    public partial class ActorWindow : Window
+    {
+        public ActorWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
